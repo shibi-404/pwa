0110467f8ff49bf675a75351974bc9facbe2f064
shibi-404 <ji.shiva1998@gmail.com>
2021-10-06 15:59:38 +0530

    localhost related bug fixed

--- sw.js
+++ sw.js
@@ -12,11 +12,11 @@ self.addEventListener("install", e => {
                     "https://fonts.googleapis.com/css?family=Roboto:400,700",
                     "https://fonts.googleapis.com/icon?family=Material+Icons",
                     "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
-                    "http://localhost/progressive_web_app/first/manifest.json",
+                    "/manifest.json",
                     "https://fonts.gstatic.com/s/roboto/v29/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
                     "https://fonts.gstatic.com/s/materialicons/v109/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-                    "http://localhost/progressive_web_app/first/src/images/icons/app-icon-144x144.png",
-                    "http://localhost/favicon.ico"
+                    "/src/images/icons/app-icon-144x144.png",
+                    "/favicon.ico"
                 ]);
         })
     );
